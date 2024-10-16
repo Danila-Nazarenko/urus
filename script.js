12542530e8f2079acea7fcb5a9ed6c805072a2a2
Danila <nazaraenko_danila@inbox.ru>
2024-10-16 19:08:46 +0300

    new commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+e1.onclick=function (params) {
+    document.getElementById("imgint").src="images/interior.png"
+}
+e2.onclick=function (params) {
+    document.getElementById("imgint").src="images/urusY.webp"
+}
+e3.onclick=function (params) {
+    document.getElementById("imgint").src="images/urusB.jpg"
+}
