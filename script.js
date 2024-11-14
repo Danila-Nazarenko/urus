f4cf6e5b8d58759a85e14948a029263c47fe39a1
Danila <nazaraenko_danila@inbox.ru>
2024-11-14 23:31:11 +0300

    new commit

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ e2.onclick=function (params) {
 e3.onclick=function (params) {
     document.getElementById("imgint").src="images/urusB.jpg"
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function () {
+        document.querySelector(".header_top").classList.add("open")
+    })
+})
